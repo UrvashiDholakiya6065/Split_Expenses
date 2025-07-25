3c2794ff9d3d212f39ca0a160a5e79417cc588f7
Urvashi <urvashidholakiya6065@gmail.com>
2025-07-25 17:20:05 +0530

    demo1

--- lib/Screens/demo1.dart
+++ lib/Screens/demo1.dart
@@ -1,66 +1,42 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:splitexpenses_app/Db_Service/db_service.dart';
-// import 'package:splitexpenses_app/Models/Group_Model/group_model.dart';
-//
-// class Demo1 extends StatefulWidget {
-//   const Demo1({super.key});
-//
-//   @override
-//   State<Demo1> createState() => _Demo1State();
-// }
-//
-// class _Demo1State extends State<Demo1> {
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//
-//     super.initState();
-//     getData();
-//   }
-//   List<GroupModel> groupList=[];
-//   Future<void> getData() async {
-//     var l = await DbService.instance.getSplit();
-//    setState(() {
-//      groupList = l.map((e) => GroupModel.fromMap(e)).toList();
-//    });
-//
-//     print("Split list::::${l.length}--${groupList.length}");
-//     // update();
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text('data'),),
-//       body: Column(
-//         children: [
-//           Text('sndkjsjkdfnsjkdnfkjsdnfknsdknfkdsnfnkds')
-//         ],
-//       ),
-//       // body: ListView.builder(
-//       //     itemCount: 0,
-//       //     itemBuilder: (context,index){
-//       //   return Text(groupList[index].name);
-//       // }),
-//     );
-//   }
-// }
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:splitexpenses_app/Db_Service/db_service.dart';
+import 'package:splitexpenses_app/Models/Group_Model/group_model.dart';
 
-class Pika extends StatefulWidget {
-  const Pika({super.key});
+class Demo1 extends StatefulWidget {
+  const Demo1({super.key});
 
   @override
-  State<Pika> createState() => _PikaState();
+  State<Demo1> createState() => _Demo1State();
 }
 
-class _PikaState extends State<Pika> {
+class _Demo1State extends State<Demo1> {
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    super.initState();
+    getData();
+  }
+  List<GroupModel> groupList=[];
+  Future<void> getData() async {
+    var l = await DbService.instance.getSplit();
+   setState(() {
+     groupList = l.map((e) => GroupModel.fromMap(e)).toList();
+   });
+
+    print("Split list::::${l.length}--${groupList.length}");
+    // update();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('sdmfkjsdkjfjksdnfkdsj'),),
+      appBar: AppBar(title: Text('data'),),
+      body: ListView.builder(
+          itemCount: groupList.length,
+          itemBuilder: (context,index){
+        return Text(groupList[index].name);
+      }),
     );
   }
 }
